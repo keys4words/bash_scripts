f39a40c4b90b211b697b7116f2624f658805fac0
keys4wordg <keys4wordg@gmail.com>
2021-01-20 20:57:52 +0300

    add scripts

---
+++ bsh/scrip1.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+echo "This is messsage from scripts folder"
+echo "Let's show files in this folder .."
+ls
+xterm %
+ls -l
+echo "Done"
---
+++ py/tut001.py
@@ -0,0 +1,10 @@
+cities = ['one', 'two', 'three', 'four']
+cities.append('five')
+print(cities)
+cities.insert(2, 'new_three')
+print(cities)
+cities.pop()
+
+print(cities)
+cities.remove('two')
+print(cities)
